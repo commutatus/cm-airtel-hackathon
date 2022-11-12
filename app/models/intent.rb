8a74c1843a69dfe01be9f3c53dc7e7badc486535
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-12 19:13:20 +0530

    Added response in model

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -2,4 +2,5 @@ class Intent < ApplicationRecord
   belongs_to :user
   belongs_to :chatbot
   has_many :utterances
+  has_many :responses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_12_131801) do
+ActiveRecord::Schema.define(version: 2022_11_12_134022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,14 @@ ActiveRecord::Schema.define(version: 2022_11_12_131801) do
     t.index ["user_id"], name: "index_intents_on_user_id"
   end
 
+  create_table "responses", force: :cascade do |t|
+    t.text "content"
+    t.bigint "intent_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["intent_id"], name: "index_responses_on_intent_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -111,5 +119,6 @@ ActiveRecord::Schema.define(version: 2022_11_12_131801) do
   add_foreign_key "chatbots", "users"
   add_foreign_key "intents", "chatbots"
   add_foreign_key "intents", "users"
+  add_foreign_key "responses", "intents"
   add_foreign_key "utterances", "intents"
 end
