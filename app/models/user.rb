65681fdebf162cda13efdde2528a2c4d15762e51
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-12 18:33:07 +0530

    Added role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {
+    user: 0,
+    admin: 1,
+    super_admin: 2
+  }
   # Remove this once role is setup and mentioned in zcm_admin.rb
-  def super_admin?
-    true
-  end
+  # def super_admin?
+  #   true
+  # end
 end
