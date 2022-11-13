e8a71fed68cf34d1aedb75fc10261f196590b213
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-13 11:46:39 +0530

    Updated policies

--- app/policies/cm_admin/chatbot_policy.rb
+++ app/policies/cm_admin/chatbot_policy.rb
@@ -1,23 +1,23 @@
 class CmAdmin::ChatbotPolicy < ApplicationPolicy
 
   def index?
-    @user.super_admin?
+    true
   end
   
   def show?
-    @user.super_admin?
+    true
   end
   
   def create?
-    @user.super_admin?
+    true
   end
   
   def update?
-    @user.super_admin?
+    true
   end
   
   def destroy?
-    @user.super_admin?
+    true
   end
   
 end
--- app/policies/cm_admin/intent_policy.rb
+++ app/policies/cm_admin/intent_policy.rb
@@ -1,31 +1,31 @@
 class CmAdmin::IntentPolicy < ApplicationPolicy
 
   def index?
-    @user.super_admin?
+    true
   end
   
   def show?
-    @user.super_admin?
+    true
   end
   
   def create?
-    @user.super_admin?
+    true
   end
   
   def update?
-    @user.super_admin?
+    true
   end
   
   def destroy?
-    @user.super_admin?
+    true
   end
 
   def utterances?
-    @user.super_admin?
+    true
   end
 
   def responses?
-    @user.super_admin?
+    true
   end
   
 end
