e0b2c17180485486849851e472b07915899d2347
shrudhinvraj <shrudhinvraj1998@gmail.com>
2022-11-14 18:40:28 +0530

    airtel-id :hammer: refactor method name

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,9 @@
 class WebhooksController < ApplicationController
-  def process(webhook)
-    # code for handling airtel payload
+
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
+
+  def process_webhook
+    render body: nil, status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  post "/webhooks/process/:webhook_source", controller: :webhooks, action: :process
+  post "webhooks/process/:webhook_source", controller: :webhooks, action: :process_webhook
 end
