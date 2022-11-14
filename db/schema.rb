a8536685921a892d2a15ceb01c66b233102a74a5
shrudhinvraj <shrudhinvraj1998@gmail.com>
2022-11-14 10:09:23 +0530

    Airtel-iq :sparkles: webhook event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_12_124003) do
+ActiveRecord::Schema.define(version: 2022_11_12_134239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 2022_11_12_124003) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "webhook_events", force: :cascade do |t|
+    t.string "source", null: false
+    t.json "payload"
+    t.string "event_type"
+    t.string "webhook_event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
