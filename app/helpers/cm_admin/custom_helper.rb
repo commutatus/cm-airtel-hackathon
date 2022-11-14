3401c9a236ea72919c6ada222a8c264910ee90e9
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-14 18:52:24 +0530

    Added role based logic in chatbot_collection

--- app/helpers/cm_admin/custom_helper.rb
+++ app/helpers/cm_admin/custom_helper.rb
@@ -1,6 +1,10 @@
 module CmAdmin::CustomHelper
   def chatbot_collection(_model, _ctx)
-    ::Chatbot.all.map{|cb| [cb.name.titleize, cb.id]}
+    if Current.user.super_admin?
+      ::Chatbot.all.map{|cb| [cb.name.titleize, cb.id]}
+    else
+      ::Chatbot.where(user: Current.user).map{|cb| [cb.name.titleize, cb.id]}
+    end
   end
 
   def user_id(_model, _ctx)
