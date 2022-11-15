48261c87b887a6a291be46891ed30f46f40d9be1
shrudhinvraj <shrudhinvraj1998@gmail.com>
2022-11-15 10:39:22 +0530

    airtel-iq :sparkles: code to save response from airtel

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,6 @@
 class WebhooksController < ApplicationController
   def process(webhook)
-    # code for handling airtel payload
+    # webhook_event_id was set not null true we can refactor it later
+    webhook_event = WebhookEvent.create(source: params[:webhook_source], payload: request.body.read, webhook_event_id: rand(10))
   end
 end
