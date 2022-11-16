7e431fda15525c4c00578780bfd70b548268d07a
shrudhinvraj <shrudhinvraj1998@gmail.com>
2022-11-16 12:11:45 +0530

    airtel-iq :hammer: refactor request accepting code

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,10 @@
 class WebhooksController < ApplicationController
 
   def process(_webhook)
-    response = JSON.parse(request.body.read)
+    response = JSON.parse(request.body).with_indifferent_access
     webhook_event = WebhookEvent.create(source: params[:webhook_source],
                                         payload: response,
-                                        session_id: response['sessionId'],
-                                        business_id: response['businessId'])
+                                        session_id: response.dig('sessionId'),
+                                        business_id: response.dig('businessId'))
   end
 end
