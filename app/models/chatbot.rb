624239de7633cb6f756902517e9224ad60f9aa85
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-19 22:02:08 +0530

    Feat: Update the bot configurations on AWS

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -7,6 +7,7 @@ class Chatbot < ApplicationRecord
   validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
 
   after_create_commit :create_on_aws
+  after_update_commit :update_on_aws
   before_destroy :delete_from_aws
 
   # Creates an Amazon Lex conversational bot and store the bot_id.
@@ -26,6 +27,24 @@ class Chatbot < ApplicationRecord
     })
 
     update(bot_id: resp.bot_id)
+  end
+
+  # Updates the configuration of an existing bot.
+  def update_on_aws
+    client = Aws::LexModelsV2::Client.new(
+      region: 'ap-southeast-1'
+    )
+
+    resp = client.update_bot({
+      bot_id: bot_id, # required
+      bot_name: name, # required
+      description: description,
+      role_arn: Rails.application.credentials.dig(:aws, :lex_role_arn), # required
+      data_privacy: { # required
+        child_directed: false, # required
+      },
+      idle_session_ttl_in_seconds: 3600, # required
+    })
   end
 
   # Deletes all versions of a bot
