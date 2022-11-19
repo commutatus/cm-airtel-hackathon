b58e402536c555256b8cb39d76387e69bdeb1787
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-19 21:48:12 +0530

    Feat: Create a Lex bot and store bot_id

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -6,4 +6,25 @@ class Chatbot < ApplicationRecord
   validates_format_of :name, with: /\A([a-zA-Z0-9_-]{1,100})\z/, message: 'can have maximum 100 characters. No space is allowed. Valid characters: A–Z, a–z, 0–9, -, _'
   validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
 
+  after_create_commit :create_on_aws
+
+  # Creates an Amazon Lex conversational bot and store the bot_id.
+  def create_on_aws
+    client = Aws::LexModelsV2::Client.new(
+      region: 'ap-southeast-1'
+    )
+
+    resp = client.create_bot({
+      bot_name: name, # required
+      description: description,
+      role_arn: Rails.application.credentials.dig(:aws, :lex_role_arn), # required
+      data_privacy: { # required
+        child_directed: false, # required
+      },
+      idle_session_ttl_in_seconds: 3600, # required
+    })
+
+    update(bot_id: resp.bot_id)
+  end
+
 end
