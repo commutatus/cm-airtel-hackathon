131e3d61e2a971a2a561d4ae20fa5094de4594a8
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-20 22:34:07 +0530

    Chore: Removed unnecessary DSL fields for chatbot

--- app/models/concerns/cm_admin/chatbot.rb
+++ app/models/concerns/cm_admin/chatbot.rb
@@ -17,8 +17,6 @@ module CmAdmin::Chatbot
           cm_show_section 'Chatbot Details' do
             field :name, label: 'Name'
             field :description, label: 'Description'
-            field :bot_id, label: 'Bot Id'
-            field :user_id, label: 'User Id'
           end
         end
       end
@@ -26,14 +24,12 @@ module CmAdmin::Chatbot
       cm_new page_title: 'Add Chatbot', page_description: 'Enter all details to add chatbot' do
         form_field :name, input_type: :string, label: 'Name'
         form_field :description, input_type: :string, label: 'Description'
-        form_field :bot_id, input_type: :string, label: 'Bot Id'
         form_field :user_id, input_type: :hidden, helper_method: :user_id
       end
 
       cm_edit page_title: 'Edit Chatbot', page_description: 'Edit details of the chatbot' do
         form_field :name, input_type: :string, label: 'Name'
         form_field :description, input_type: :string, label: 'Description'
-        form_field :bot_id, input_type: :string, label: 'Bot Id'
         form_field :user_id, input_type: :hidden, helper_method: :user_id
       end
     end
