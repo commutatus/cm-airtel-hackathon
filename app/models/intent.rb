f13eff588eecd0c889bf6eba79ca717ce34b4f96
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-22 17:02:29 +0530

    Feat: Format validation for name and description

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -7,4 +7,7 @@ class Intent < ApplicationRecord
   belongs_to :chatbot
   has_many :utterances, dependent: :destroy
   has_many :responses, dependent: :destroy
+
+  validates_format_of :name, with: /\A([a-zA-Z0-9_-]{1,100})\z/, message: 'can have maximum 100 characters. No space is allowed. Valid characters: A–Z, a–z, 0–9, -, _'
+  validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
 end
