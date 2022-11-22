2e95938ba579d931d1268982d38dffdfd8370b75
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-22 17:10:57 +0530

    Feat: Create an intent and store the intent_id

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -10,4 +10,23 @@ class Intent < ApplicationRecord
 
   validates_format_of :name, with: /\A([a-zA-Z0-9_-]{1,100})\z/, message: 'can have maximum 100 characters. No space is allowed. Valid characters: A–Z, a–z, 0–9, -, _'
   validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
+
+  after_create_commit :create_intent
+
+  # Creates an intent and store the intent_id.
+  def create_intent
+    client = Aws::LexModelsV2::Client.new(
+      region: 'ap-southeast-1',
+    )
+
+    resp = client.create_intent({
+      bot_id: chatbot.bot_id, # required
+      bot_version: 'DRAFT', # required
+      locale_id: chatbot.locale_id, # required
+      intent_name: name, # required
+      description: description,
+    })
+
+    update_column('intent_id', resp.intent_id)
+  end
 end
