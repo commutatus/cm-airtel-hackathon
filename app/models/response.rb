83fb3086d263cac0fa361655a3b59215e9afc208
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-23 17:28:19 +0530

    Feat: Validation of max limit per intent

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,4 +4,12 @@ class Response < ApplicationRecord
   include CmAdmin::Response
 
   belongs_to :intent
+
+  validate :max_limit_per_intent
+
+  def max_limit_per_intent
+    if intent.responses.size >= 3
+      errors.add :less_than_or_equal_to_3, 'responses can be added per intent. Delete or edit the existing response(s).'
+    end
+  end
 end
