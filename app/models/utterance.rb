96289c2f73f3c81cb83bd01001d0c601b1cec001
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-23 20:29:05 +0530

    Feat: Duplicate utterance validation across intent

--- app/models/utterance.rb
+++ app/models/utterance.rb
@@ -5,4 +5,14 @@ class Utterance < ApplicationRecord
   include AwsLex::Utterance
 
   belongs_to :intent
+
+  validate :duplicate_utterance
+
+  def duplicate_utterance
+    utterances = Utterance.joins(intent: :chatbot).where(chatbot: {id: intent.chatbot_id}).pluck(:content)
+    if utterances.include?(content)
+      duplicate_intent = Intent.joins(:utterances).where(utterances: {content: content}).pluck(:name).to_sentence
+      errors.add :duplicate, "utterance. It must be unique across all intents in the chatbot. The utterance '#{content}' exists in the following intent(s): #{duplicate_intent}. Make sure all of your utterances are unique and try your request again."
+    end
+  end
 end
