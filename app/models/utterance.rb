45d910eadfcc21361eddad4368e905bba6b79034
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-23 17:29:03 +0530

    Feat: Configure the callback actions for utterance

--- app/models/utterance.rb
+++ app/models/utterance.rb
@@ -4,4 +4,11 @@ class Utterance < ApplicationRecord
   include CmAdmin::Utterance
 
   belongs_to :intent
+
+  after_commit :modify_utterance, on: [:create, :update, :destroy]
+
+  # Updates the sample utterances for an intent.
+  def modify_utterance
+    LexModelsV2::Intent.new(self.intent).update_intent
+  end
 end
