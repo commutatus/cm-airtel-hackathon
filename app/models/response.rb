8b29893d1ba53e6a06e2851eaac5779e6bcd5f93
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-23 17:29:14 +0530

    Feat: Configure the callback actions for response

--- app/models/response.rb
+++ app/models/response.rb
@@ -7,9 +7,16 @@ class Response < ApplicationRecord
 
   validate :max_limit_per_intent
 
+  after_commit :modify_response, on: [:create, :update, :destroy]
+
   def max_limit_per_intent
     if intent.responses.size >= 3
       errors.add :less_than_or_equal_to_3, 'responses can be added per intent. Delete or edit the existing response(s).'
     end
+  end
+
+  # Updates the success responses for an intent.
+  def modify_response
+    LexModelsV2::Intent.new(self.intent).update_intent
   end
 end
