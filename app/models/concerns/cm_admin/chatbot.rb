4b2fb449031505020812fcb5dd6a917868932da9
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-24 18:38:26 +0530

    Feat: Add WhatsApp business number in chatbot

--- app/models/concerns/cm_admin/chatbot.rb
+++ app/models/concerns/cm_admin/chatbot.rb
@@ -10,6 +10,8 @@ module CmAdmin::Chatbot
         filter [:name], :search, placeholder: 'Search'
 
         column :name
+        column :description
+        column :phone_number, header: 'WhatsApp business number'
       end
 
       cm_show page_title: :name, page_description: 'Chatbot Details' do
@@ -17,6 +19,7 @@ module CmAdmin::Chatbot
           cm_show_section 'Chatbot Details' do
             field :name
             field :description
+            field :phone_number, label: 'WhatsApp business number'
           end
         end
       end
@@ -24,12 +27,14 @@ module CmAdmin::Chatbot
       cm_new page_title: 'Add Chatbot', page_description: 'Enter all details to add chatbot' do
         form_field :name, input_type: :string
         form_field :description, input_type: :string
+        form_field :phone_number, input_type: :string, label: 'WhatsApp business number'
         form_field :user_id, input_type: :hidden, helper_method: :user_id
       end
 
       cm_edit page_title: 'Edit Chatbot', page_description: 'Edit details of the chatbot' do
         form_field :name, input_type: :string
         form_field :description, input_type: :string
+        form_field :phone_number, input_type: :string, label: 'WhatsApp business number'
         form_field :user_id, input_type: :hidden, helper_method: :user_id
       end
     end
