e51e5bf61ab48d4c23804d4ea4d3cc05d8d26e6b
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-24 19:58:06 +0530

    Feat: Enum values for intent_type

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -9,6 +9,11 @@ class Intent < ApplicationRecord
   has_many :utterances, dependent: :destroy
   has_many :responses, dependent: :destroy
 
+  enum intent_type: {
+    built_in: 'built_in',
+    custom: 'custom'
+  }
+
   validates :name, uniqueness: { scope: :chatbot_id }
   validates_format_of :name, with: /\A([a-zA-Z0-9_-]{1,100})\z/, message: 'can have maximum 100 characters. No space is allowed. Valid characters: A–Z, a–z, 0–9, -, _'
   validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
