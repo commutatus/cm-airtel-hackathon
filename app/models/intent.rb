f255ff850ed8463c41c0ea6b72eaf4afd0c7a97b
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-25 17:23:36 +0530

    Chore: Remove the before destroy callback

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -17,10 +17,4 @@ class Intent < ApplicationRecord
   validates :name, uniqueness: { scope: :chatbot_id }
   validates_format_of :name, with: /\A([a-zA-Z0-9_-]{1,100})\z/, message: 'can have maximum 100 characters. No space is allowed. Valid characters: A–Z, a–z, 0–9, -, _'
   validates_format_of :description, with: /\A(.{,200})\z/, message: 'can have maximum 200 characters.'
-
-  before_destroy :check_intent_type
-
-  def check_intent_type
-    raise 'Built-in intent can\'t be deleted.'
-  end
 end
