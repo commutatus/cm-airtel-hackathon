26b773e15799ac58ef050b42264c935775f6a259
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-26 16:05:39 +0530

    Fix: Add failed status to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_24_110757) do
+ActiveRecord::Schema.define(version: 2022_11_24_142544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
