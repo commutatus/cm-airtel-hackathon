3510e82a3af32c4dcb036ed5c07134a3db6ef776
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-12-03 19:58:55 +0530

    Fix link chatbot with message (#29)
    * Fix: Store the chatbot_id with message object
    
    * Chore: Code indentation

--- app/models/webhook_event.rb
+++ app/models/webhook_event.rb
@@ -7,12 +7,24 @@ class WebhookEvent < ApplicationRecord
 
   def update_message_entry
     message_entry = Message.find_or_create_by(message_id: payload['messageId'])
+
+    whatsapp_business_number = case payload['msgStream']
+                               when 'OUTBOUND'
+                                 country_code = payload['sourceCountry'].split('+').last
+                                 payload['sourceAddress'].split(country_code).last
+                               when 'INBOUND'
+                                 country_code = payload['recipientCountry'].split('+').last
+                                 payload['recipientAddress'].split(country_code).last
+                               end
+    chatbot_id = Chatbot.find_by(phone_number: whatsapp_business_number).try(:id)
+
     message_entry.update(from: payload['sourceAddress'],
-                                    to: payload['recipientAddress'],
-                                    status: payload['msgStatus'].underscore,
-                                    text: payload.dig('messageParameters', 'text',
-                                                      'body').presence || message_entry.text,
-                                    session_id: payload['sessionId'],
-                                    message_type: payload['msgStream'].underscore)
+                         to: payload['recipientAddress'],
+                         status: payload['msgStatus'].underscore,
+                         text: payload.dig('messageParameters', 'text', 'body').presence || message_entry.text,
+                         session_id: payload['sessionId'],
+                         message_type: payload['msgStream'].underscore,
+                         chatbot_id: chatbot_id
+                        )
   end
 end
